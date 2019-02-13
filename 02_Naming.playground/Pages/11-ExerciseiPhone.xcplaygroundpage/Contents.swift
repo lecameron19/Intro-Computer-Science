e7ac93b787385bea759457c5160d1729fc1cc080
Lauren Cameron19 <lecameron19@AIR-WW5VC.local>
2019-02-13 14:34:54 -0600

    Added basic and naming playgrounds

---
+++ 02_Naming.playground/Pages/11-ExerciseiPhone.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+/*:
+ ## Exercise: What Fits on Your iPhone?
+ 
+ In this exercise you’re going to work on figuring out the answer to the timeless question: How much stuff can I fit on my iPhone?
+ 
+ Unlike the previous exercises, no code will be provided. But here are a few things you’ll need to know:
+ 
+ 
+ - iPhone storage capacity is measured in **gigabytes** (GB).
+ - The iPhone in question has 8GB of storage.
+ - A gigabyte is about **1000 megabytes** (MB)
+ - The phone already has **3GB** of stuff on it
+ - **One minute** of video takes **150MB** of storage
+ 
+ - callout(Exercise): How many minutes of video will it take to fill the phone?\
+ _Hint_: Do all of your calculations in megabytes (MB).
+*/
+// Definition of variables
+let totalPhoneStorage = 8000
+let usedPhoneStorage = 3000
+let availablePhoneStorage = (totalPhoneStorage - usedPhoneStorage)
+let requiredVideoStorage = 150
+let availableMinues = (totalPhoneStorage - usedPhoneStorage) / requiredVideoStorage
+
+
+
+//:[Previous](@previous)  |  page 12 of 14  |  [Next: Exercise: Fixing Your Morning](@next)
